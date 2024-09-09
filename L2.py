b5e96a23cea5a8d78e02aed35a788313d885e157
StrawberryCreator <Sebbechewvanderkolk@icloud.com>
2024-09-09 16:18:22 +0200

    Add files via upload

---
+++ L2.py
@@ -0,0 +1,18 @@
+print ("-"*50)
+#numbers (0 to 20)
+x = 0
+while x <= 20:
+    print (x)
+    x += 1
+print ("-"*50)
+#odd numbers (1 to 19)
+x = 1
+while x <= 19:
+    print (x)
+    x += 2
+print ("-"*50)
+#odd numbers in reverse (19 to 1)
+x = 19
+while x >= 1:
+    print (x)
+    x -= 2
