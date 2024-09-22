c68daee27b31f5d1ef7a029c09300f7c1cdc1a50
StrawberryCreator <Sebbechewvanderkolk@icloud.com>
2024-09-22 22:01:20 +0200

    Delete H1.py

--- H1.py
+++
@@ -1,8 +0,0 @@
-print("-"*50)
-table = int(input("What table do you want to see? "))
-print("-"*50)
-num = 1
-for i in range (table ,table * 10 + 1, table):
-    print(num, "*", table, "=", i)
-    num = num + 1
-print("-"*50)
